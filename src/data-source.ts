b87122a0f16c2bfbcb654f5eb8af8ee4f9457c13
Dubai-lab <eg8217178@gmail.com>
2025-10-27 06:41:29 +0200

    yy

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,16 +8,15 @@ const isProd = process.env.NODE_ENV === 'production';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   ...(process.env.DATABASE_URL
-    ? { url: process.env.DATABASE_URL }
+    ? { url: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
     : {
         host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT || '5432', 10),
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
+        ssl: isProd ? { rejectUnauthorized: false } : false,
       }),
-
-  ssl: isProd ? { rejectUnauthorized: false } : false,
 
   synchronize: false,
   migrationsRun: true,
