a85839b8513237a8d88920c7bd810c76e048798e
Dubai-lab <eg8217178@gmail.com>
2025-10-27 09:35:19 +0200

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,17 +16,11 @@ import { AdminModule } from './modules/admin/admin.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
       autoLoadEntities: true,
       synchronize: false,
-      migrationsRun: true,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      migrationsRun: false,
     }),
 
     AuthModule,
