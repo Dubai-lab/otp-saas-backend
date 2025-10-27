da040578ac71c594284eace54075becfb88182da
Dubai-lab <eg8217178@gmail.com>
2025-10-27 05:49:16 +0200

    jj

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -9,18 +8,16 @@ const isProd = process.env.NODE_ENV === 'production';
 export const AppDataSource = new DataSource({
   type: 'postgres',
 
-  ...(isProd
-    ? {
-        url: process.env.DATABASE_URL,
-        ssl: true, // ✅ simple flag works fine on Render
-      }
-    : {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '5432', 10),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-      }),
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+
+  ssl: isProd,
+
+  synchronize: false,
+  migrationsRun: true,
 
   entities: isProd
     ? ['dist/modules/**/*.entity.js']
@@ -28,7 +25,5 @@ export const AppDataSource = new DataSource({
 
   migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
 
-  synchronize: false,
-  migrationsRun: true,
   logging: false,
 });
