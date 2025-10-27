0cf3fa3b15e0aef6a7b4c89cce27c99583201722
Dubai-lab <eg8217178@gmail.com>
2025-10-27 07:20:58 +0200

    jh

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,10 +6,19 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT || '5432'}/${process.env.DB_DATABASE}?sslmode=require`,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['src/modules/**/*.entity.{ts,js}'],
   migrations: ['src/migrations/*.{ts,js}'],
   synchronize: false,
   logging: true,
-  ssl: { rejectUnauthorized: false },
+  extra: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
