0582ae390d194320de60953ac46896a9bd429429
Dubai-lab <eg8217178@gmail.com>
2025-10-27 07:10:41 +0200

    ok

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,5 +15,5 @@ export const AppDataSource = new DataSource({
   migrations: ['src/migrations/*.{ts,js}'],
   synchronize: false,
   logging: true,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
